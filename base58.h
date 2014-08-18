9dc2bf2f6a83d52275d71ed9429b528fa0080c46
Luke Dashjr <luke-jr+git@utopios.org>
2014-08-18 14:52:20 +0000

    Include extern "C" wrapper in base58.h so it can be used from C++ code

--- base58.h
+++ base58.h
@@ -4,6 +4,10 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern bool (*b58_sha256_impl)(void *, const void *, size_t);
 
 extern bool b58tobin(void *bin, size_t binsz, const char *b58, size_t b58sz);
@@ -11,5 +15,9 @@ extern int b58check(const void *bin, size_t binsz, const char *b58, size_t b58sz
 
 extern bool b58enc(char *b58, size_t *b58sz, const void *bin, size_t binsz);
 extern bool b58check_enc(char *b58c, size_t *b58c_sz, uint8_t ver, const void *data, size_t datasz);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
